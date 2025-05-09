1c2d77c9ef103e6478527b56d72368a576a5139c
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-09 15:34:11 +0700

    playing with inertia links

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,13 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'name' => 'Henry Baharudin Yusuf',
-        'frameworks' => [
-            'Laravel',
-            'Livewire',
-            'InertiaJS',
-            'VueJS',
-            'ReactJS',
-            'AlpineJS',
-            'TailwindCSS',
-        ],
-    ]);
+    return Inertia::render('Home');
+});
+
+Route::get('/users', function () {
+    return Inertia::render('Users');
+});
+
+Route::get('/settings', function () {
+    return Inertia::render('Settings');
 });
