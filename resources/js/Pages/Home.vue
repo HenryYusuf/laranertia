2f80f12473bf0c0bf66678a1aeb64080a2db0822
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-09 15:19:55 +0700

    basic pages

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Hello {{ name }}</h1>
+    <p>Frameworks</p>
+    <ul>
+        <li v-for="framework of frameworks">{{ framework }}</li>
+    </ul>
+</template>
+
+<script setup>
+defineProps({
+    name: {
+        type: String,
+        default: "World",
+    },
+    frameworks: {
+        type: Array,
+        default: () => ["Vue", "React", "Svelte"],
+    },
+});
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia::render('Home', [
+        'name' => 'Henry Baharudin Yusuf',
+        'frameworks' => [
+            'Laravel',
+            'Livewire',
+            'InertiaJS',
+            'VueJS',
+            'ReactJS',
+            'AlpineJS',
+            'TailwindCSS',
+        ],
+    ]);
 });
