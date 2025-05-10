f5e6ef31b422059b58a58c8f2f0790f66714ab29
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-10 14:48:14 +0700

    creating layout

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,19 +1,18 @@
 <template>
-    <h1 class="text-4xl font-bold">Users</h1>
-    <Nav />
-
-    <div class="mt-[800px]">
-        <p>The current time is {{ time }}.</p>
-
-        <Link href="/users" class="text-blue-500" preserve-scroll>
-            Refresh
-        </Link>
-    </div>
+    <Layout>
+        <h1 class="text-3xl">Users</h1>
+        <div class="mt-[800px]">
+            <p>The current time is {{ time }}.</p>
+            <Link href="/users" class="text-blue-500" preserve-scroll>
+                Refresh
+            </Link>
+        </div>
+    </Layout>
 </template>
 
 <script setup>
-import Nav from "@/Shared/Nav.vue";
 import { Link } from "@inertiajs/vue3";
+import Layout from "../Shared/Layout.vue";
 
 defineProps({
     time: {
---
+++ resources/js/Shared/Layout.vue
@@ -0,0 +1,19 @@
+<template>
+    <section class="p-6 bg-gray-200">
+        <header class="flex justify-between">
+            <h1 class="font-bold text-lg">Laranertia</h1>
+
+            <Nav />
+        </header>
+    </section>
+
+    <section class="p-6">
+        <div class="max-w-3xl mx-auto">
+            <slot />
+        </div>
+    </section>
+</template>
+
+<script setup>
+import Nav from "./Nav.vue";
+</script>
