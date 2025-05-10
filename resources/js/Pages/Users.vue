b7644693c32c1c6169d9ea7f736185f892460656
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-10 18:04:25 +0700

    persistent layout

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,18 +1,20 @@
 <template>
-    <Layout>
-        <h1 class="text-3xl">Users</h1>
-        <div class="mt-[800px]">
-            <p>The current time is {{ time }}.</p>
-            <Link href="/users" class="text-blue-500" preserve-scroll>
-                Refresh
-            </Link>
-        </div>
-    </Layout>
+    <h1 class="text-3xl">Users</h1>
+    <div class="mt-[800px]">
+        <p>The current time is {{ time }}.</p>
+        <Link href="/users" class="text-blue-500" preserve-scroll>
+            Refresh
+        </Link>
+    </div>
 </template>
 
 <script setup>
 import { Link } from "@inertiajs/vue3";
 import Layout from "../Shared/Layout.vue";
+
+defineOptions({
+    layout: Layout,
+});
 
 defineProps({
     time: {
