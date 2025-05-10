0dd834d9fca58add3a9d5db6a10741f7d9be4c18
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-10 14:35:19 +0700

    active links

---
+++ resources/js/Shared/NavLink.vue
@@ -0,0 +1,20 @@
+<template>
+    <Link
+        class="text-blue-500 hover:underline"
+        :class="{
+            'font-bold underline': active,
+        }"
+        ><slot
+    /></Link>
+</template>
+
+<script setup>
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    active: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
