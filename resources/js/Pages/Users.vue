9361f3a872206fbb742b2e404e26a4c4d48cf4a7
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-10 19:06:05 +0700

    SPA security concerns

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -4,19 +4,15 @@
         <meta name="description" content="Users page" head-key="description" />
     </Head>
     <h1 class="text-3xl">Users</h1>
-    <div class="mt-[800px]">
-        <p>The current time is {{ time }}.</p>
-        <Link href="/users" class="text-blue-500" preserve-scroll>
-            Refresh
-        </Link>
-    </div>
+    <ul>
+        <li v-for="user in users" :key="user.id">{{ user.name }}</li>
+    </ul>
 </template>
 
 <script setup>
 defineProps({
-    time: {
-        type: String,
-        required: true,
+    users: {
+        type: Array,
     },
 });
 </script>
