06f8670dfcfbf2237ab840ce9219b39123ec24f7
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2025-05-10 14:21:42 +0700

    preserve scroll position

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,8 +1,24 @@
 <template>
-    <h1>Users</h1>
+    <h1 class="text-4xl font-bold">Users</h1>
     <Nav />
+
+    <div class="mt-[800px]">
+        <p>The current time is {{ time }}.</p>
+
+        <Link href="/users" class="text-blue-500" preserve-scroll>
+            Refresh
+        </Link>
+    </div>
 </template>
 
 <script setup>
 import Nav from "@/Shared/Nav.vue";
+import { Link } from "@inertiajs/vue3";
+
+defineProps({
+    time: {
+        type: String,
+        required: true,
+    },
+});
 </script>
